214a4e889beace12e926d092eebf2184a9ac278a
RafaelxSouto <contato@rafaelxsouto.com.br>
2026-04-18 13:38:38 -0300

    get, post e construindo chatbot random

---
+++ chatbot.py
@@ -0,0 +1,12 @@
+# Código que cria um chatbot simples em Python
+
+import tkinter as tk #Biblioteca que constroi interface gráfica
+import random #Biblioteca de aleatóriedade.
+
+# Respostas simples
+
+respostas = {
+    "oi": [
+        "Olá!", "Oi! Tudo Bem?", "E aí!"
+    ]
+}
